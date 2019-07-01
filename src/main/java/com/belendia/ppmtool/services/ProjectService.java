7c52631c04fc0180d3b432bb6789c48516f7e24c
Belendia Abdissa <belendia@gmail.com>
2019-07-01 08:25:01 +0300

    Add ProjectTask and Backlog, and create relationship between Backlog and Project

--- src/main/java/com/belendia/ppmtool/services/ProjectService.java
+++ src/main/java/com/belendia/ppmtool/services/ProjectService.java
@@ -3,8 +3,10 @@ package com.belendia.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.belendia.ppmtool.domain.Backlog;
 import com.belendia.ppmtool.domain.Project;
 import com.belendia.ppmtool.exceptions.ProjectIdentifierException;
+import com.belendia.ppmtool.repositories.BacklogRepository;
 import com.belendia.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -13,10 +15,28 @@ public class ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
 	
+	@Autowired
+	private BacklogRepository backlogRepository;
+	
 	public Project saveOrUpdateProject(Project project) {
 		
 		try {
 			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			
+			if(project.getId() == null) {
+				Backlog backlog = new Backlog();
+				//set the relationship
+				project.setBacklog(backlog);
+				backlog.setProject(project);
+				backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			}
+			
+			//While updating, if the client doesn't send the backlog together with the project, the backlog field will be set to null...
+			// not on the database but on the hibernate side. To solve this, use the below code
+			if(project.getId() != null) {
+				project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+			}
+			
 			return projectRepository.save(project);
 		} catch (Exception e) {
 			throw new ProjectIdentifierException("Project Identifier '" + project.getProjectIdentifier() + "' already exists");
